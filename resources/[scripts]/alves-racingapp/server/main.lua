349fa55525a5c424e2486deb5fe1150d98ea1418
Ellyson Alves <ellyson.alves.ds@gmail.com>
2026-05-11 23:36:13 -0300

    fix

--- resources/[scripts]/alves-racingapp/server/main.lua
+++ resources/[scripts]/alves-racingapp/server/main.lua
@@ -7,16 +7,14 @@ print('^2[Alves Racing]^0 Server iniciado!')
 -- Apenas veículos Classe S (Super Cars)
 local QuickRaceVehicles = {
     -- JFx Super Cars
-    'arbitergtc', 'arbitergtn', 'carrion', 'carrionmech', 'cazador', 'cazadortcr',
+    'arbitergtc', 'arbitergtn', 'cazador', 'cazadortcr',
     'clubr', 'clubrhyc', 'elegyrh8c', 'elegyxa19', 'elegyxa19ven', 'flashgrs',
     'growlerc', 'gstasp3', 'gstbanac1', 'gstbanac1b', 'gstbanac1c',
-    'gstbisc1', 'gstbisc1b', 'gstbisc1c', 'gstcdy2', 'gstcdy2b', 'gstcdy2c', 'gstcdy2d',
-    'gstcs24', 'gstevmr1', 'gstingnt1', 'gstingnt1b', 'gstpaladingt', 'gstpenf1',
-    'gstpmp7s1', 'gstpmp7s1b', 'gstpmp7s1c', 'gstpmp7s1d', 'gstraid3', 'gstrh5s2',
-    'gstsadlt5', 'gstsettimo1', 'gstslt1', 'gstsrs1', 'gsttorle1', 'gstturo1',
+      'gstevmr1', 'gstpaladingt', 'gstpenf1',
+    'gstpmp7s1', 'gstpmp7s1b', 'gstpmp7s1c', 'gstpmp7s1d', 'gstraid3', 'gstrh5s2', 'gstsettimo1', 'gsttorle1', 'gstturo1',
     'gstvanguard1b', 'gstxsajest3reptile', 'gstyc1', 'hellfirec', 'nwjester',
-    's790', 'schlag', 'shenron', 'shinobid', 'sr8', 'str', 'strcoupe',
-    'taurion', 'trager', 'tragmech', 'vulture',
+    's790', 'schlag',  'sr8', 'str', 'strcoupe',
+     'vulture',
 }
 
 -- Sistema de Tiers ELO
